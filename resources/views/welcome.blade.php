ae5792c2857c9148bb613bbfcef4405a70dc8879
Maris Jankovskis <m.jankovskis@dyninno.lv>
2016-07-05 12:00:03 +0300

    Sqlite and some updates

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,48 @@
+
 <html>
-	<head>
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+<head>
+	<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
-
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			height: 100%;
+			color: #2B2B2B;
+			display: table;
+			font-weight: 100;
+			font-family: 'Lato';
+		}
+		.container {
+			text-align: center;
+			display: table-cell;
+			vertical-align: middle;
+		}
+		.content {
+			text-align: center;
+			display: inline-block;
+		}
+		.title {
+			font-size: 130px;
+			margin-bottom: 40px;
+			text-shadow: 2px 2px 2px #D3D3D3;
+		}
+		.quote {
+			font-size: 24px;
+			color: #191919;
+		}
+	</style>
+</head>
+<body>
+<div class="container">
+	<div class="header">
+		Portfolio
+	</div>
+	<div class="content">
+		<div class="title">waplet</div>
+		<div class="quote">living in a great world</div>
+	</div>
+</div>
+</body>
 </html>
