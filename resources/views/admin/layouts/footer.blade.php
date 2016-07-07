9a697aaeaf3da5e51ff8203ac723803c99a62dfa
Maris Jankovskis <m.jankovskis@dyninno.lv>
2016-07-07 12:33:07 +0300

    Removal of unnecessary footer

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,8 +1,6 @@
     <footer>
         <div class="">
-            <p class="pull-right">Bootstrap 3 template by <a>Kimlabs</a>. | Laravel 5 Implementation by <a href="http://www.tutorials.kode-blog.com/laravel-5-admin-panel" target="_blank">Kode Blog Tutorials</a>
-                <span class="lead"> <i class="fa fa-paw"></i> Larashop Admin</span>
-            </p>
+            <p class="pull-right">Admin panel</p>
         </div>
         <div class="clearfix"></div>
     </footer>
