9eb7aee2537b2fa9b6cb84134b215dbf9c9f90f1
Maris Jankovskis <m.jankovskis@dyninno.lv>
2016-07-07 11:59:12 +0300

    Template fixed

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -12,21 +12,18 @@
 
         <!-- Bootstrap core CSS -->
 
-        <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+        <link href="{{asset('vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
+        <link href="{{asset('vendors/bootstrap-daterangepicker/daterangepicker.css')}}" rel="stylesheet">
+        <link href="{{asset('vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css')}}" rel="stylesheet">
 
-        <link href="{{asset('fonts/css/font-awesome.min.css')}}" rel="stylesheet">
-        <link href="{{asset('css/animate.min.css')}}" rel="stylesheet">
+        <link href="{{asset('vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+        <link href="{{asset('vendors/animate.css/animate.min.css')}}" rel="stylesheet">
 
         <!-- Custom styling plus plugins -->
         <link href="{{asset('css/custom.css')}}" rel="stylesheet">
-        <link href="{{asset('css/icheck/flat/green.css')}}" rel="stylesheet">
-        <link href="{{asset('css/datatables/tools/css/dataTables.tableTools.css')}}" rel="stylesheet">
+        <link href="{{asset('vendors/iCheck/skins/flat/green.css')}}" rel="stylesheet">
 
-        <script src="{{asset('js/jquery.min.js')}}"></script>
-
-        <!--[if lt IE 9]>
-            <script src="../assets/js/ie8-responsive-file-warning.js"></script>
-            <![endif]-->
+        <script src="{{asset('vendors/jquery/dist/jquery.min.js')}}"></script>
 
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!--[if lt IE 9]>
@@ -55,7 +52,7 @@
                         <!-- menu prile quick info -->
                         <div class="profile">
                             <div class="profile_pic">
-                                <img src="{{asset('images/rodrick.png')}}" alt="..." class="img-circle profile_img">
+                                <img src="{{asset('images/user.png')}}" alt="..." class="img-circle profile_img">
                             </div>
                             <div class="profile_info">
                                 <span>Welcome,</span>
@@ -142,7 +139,7 @@
                             <ul class="nav navbar-nav navbar-right">
                                 <li class="">
                                     <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
-                                        <img src="{{asset('images/rodrick.png')}}" alt="">Rodrick Kazembe
+                                        <img src="{{asset('images/user.png')}}" alt="">Rodrick Kazembe
                                         <span class=" fa fa-angle-down"></span>
                                     </a>
                                     <ul class="dropdown-menu dropdown-usermenu animated fadeInDown pull-right">
@@ -255,22 +252,18 @@
             <div id="notif-group" class="tabbed_notifications"></div>
         </div>
 
-        <script src="{{asset('js/bootstrap.min.js')}}"></script>
+        <script src="{{asset('vendors/bootstrap/dist/js/bootstrap.min.js')}}"></script>
 
         <!-- chart js -->
-        <script src="{{asset('js/chartjs/chart.min.js')}}"></script>
+        <script src="{{asset('vendors/Chart.js/dist/Chart.min.js')}}"></script>
         <!-- bootstrap progress js -->
-        <script src="{{asset('js/progressbar/bootstrap-progressbar.min.js')}}"></script>
-        <script src="{{asset('js/nicescroll/jquery.nicescroll.min.js')}}"></script>
+        <script src="{{asset('vendors/bootstrap-progressbar/bootstrap-progressbar.min.js')}}"></script>
         <!-- icheck -->
-        <script src="{{asset('js/icheck/icheck.min.js')}}"></script>
+        <script src="{{asset('vendors/iCheck/icheck.min.js')}}"></script>
+
+        <script src="{{asset('vendors/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
 
         <script src="{{asset('js/custom.js')}}"></script>
-
-
-        <!-- Datatables -->
-        <script src="{{asset('js/datatables/js/jquery.dataTables.js')}}"></script>
-        <script src="{{asset('js/datatables/tools/js/dataTables.tableTools.js')}}"></script>
         <script>
 $(document).ready(function () {
     $('input.tableflat').iCheck({
@@ -281,23 +274,6 @@ $(document).ready(function () {
 
 var asInitVals = new Array();
 $(document).ready(function () {
-    var oTable = $('#example').dataTable({
-        "oLanguage": {
-            "sSearch": "Search all columns:"
-        },
-        "aoColumnDefs": [
-            {
-                'bSortable': false,
-                'aTargets': [0]
-            } //disables sorting for column one
-        ],
-        'iDisplayLength': 12,
-        "sPaginationType": "full_numbers",
-        "dom": 'T<"clear">lfrtip',
-        "tableTools": {
-            "sSwfPath": "{{asset('js/Datatables/tools/swf/copy_csv_xls_pdf.swf')}}"
-        }
-    });
     $("tfoot input").keyup(function () {
         /* Filter on the column based on the index of this element's parent <th> */
         oTable.fnFilter(this.value, $("tfoot th").index($(this).parent()));
