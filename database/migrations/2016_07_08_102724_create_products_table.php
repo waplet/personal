158bac134bd2065941f8782751a7f11c9d6f9d9e
Maris Jankovskis <m.jankovskis@dyninno.lv>
2016-07-08 13:41:34 +0300

    Added product migration

---
+++ database/migrations/2016_07_08_102724_create_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('price');
+            $table->tinyInteger('is_available')->unsigned();
+            $table->text('url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('products');
+    }
+}
