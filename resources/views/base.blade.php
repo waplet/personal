e87bf0faf83ca99d6a0904e85408d2c9508a23f5
Maris Jankovskis <maarisjankovskis@gmail.com>
2016-08-26 00:42:42 +0300

    localizing fonts

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -26,8 +26,5 @@
     <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
-    <!-- Fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 </body>
 </html>
