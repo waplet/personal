0aba188e2e60b8aa01c06f1829cf16dda9884b41
Maris Jankovskis <m.jankovskis@dyninno.lv>
2016-08-25 15:56:40 +0300

    url on header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header>
     <div class="header">
-        <h1 class="header__title">waplet</h1>
+        <a href="{{ url('/') }}"><h1 class="header__title">waplet</h1></a>
         <h2 class="header__subtitle">living in a great world</h2>
     </div>
 </header>
