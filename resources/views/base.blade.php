8c28990199918a5911e35ea2ec205b67e3fe178b
Maris Jankovskis <maarisjankovskis@gmail.com>
2016-08-25 23:49:12 +0300

    link fix

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -24,8 +24,8 @@
     @yield('content')
 
     <!-- Scripts -->
-    <script src="https//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="https//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
     <!-- Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
