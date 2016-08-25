9a169319c34bf670ef02d6f3b58e7baa4e4d96bc
Maris Jankovskis <m.jankovskis@dyninno.lv>
2016-08-25 20:12:32 +0300

    waplet home

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -9,10 +9,6 @@
     <link href="/css/app.css" rel="stylesheet">
     <link href="/css/personal.css" rel="stylesheet">
 
-    <!-- Fonts -->
-    <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-    <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -28,7 +24,10 @@
     @yield('content')
 
     <!-- Scripts -->
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="https//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <!-- Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 </body>
 </html>
--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -1,22 +1,55 @@
 @extends('base')
 
 @section('content')
-    <div class="col-xs-6 col-xs-offset-3">
+    <div class="col-xs-4 col-xs-offset-4">
         <!--  https://www.iconfinder.com/iconsets/social-flat -->
-        <div class="row text-center">
-            <a href="{{ url('http://github.com/waplet') }}">
-                <img class="contact__icon" src="{{ asset('images/github.png') }}"/>
-            </a>
+        <div class="row contact_row">
+            <p>Hello,</p>
+            <p>My name is <b>Māris</b>, but on the world wide web I prefer naming myself <b>waplet</b>.</p>
+            <p>In 2016 I graduated University of Latvia, Faculty of Computing - <b>BSc Computer Science</b>
+            <hr/>
+            <p>I have made my career as a <b>Web Developer</b> and topics I am involved in are: <b>PHP, MySQL</b>,
+                <span class="half-bold">Symfony, Laravel, Linux</span>, payment gateways, Python, etc.
+                Basically everything that is or may come handy as a web developer.
+                But that does not mean that I do not want to learn new things. I would really like to try my hand at electronics/hardware.
+            </p>
+            <p>As a developer I like to create my everyday life easier. All of my open-source apps can be found at my
+                <a href="{{ url('http://github.com/waplet') }}">github</a> profile. But I have listed my works also
+                in <a href="{{ url('/portfolio') }}">portfolio</a> section</p>
+            <hr/>
+            <p>If You find me interesting, You can always contact me in any of social sites listed below</p>
         </div>
-        <div class="row text-center">
-            <a href="{{ url('http://facebook.com/waplet') }}">
-                <img class="contact__icon" src="{{ asset('images/facebook.png') }}"/>
-            </a>
+        <div class="row contact_row">
+            <div class="contact_row__block">
+                <a href="{{ url('http://github.com/waplet') }}">
+                    <img class="contact_row__block__icon" src="{{ asset('images/github.png') }}"/>
+                </a>
+                <div class="contact_row__block__slider slider-github">Github</div>
+            </div>
         </div>
-        <div class="row text-center">
-            <a href="{{ url('http://twitter.com/@waplet') }}">
-                <img class="contact__icon" src="{{ asset('images/twitter.png') }}"/>
-            </a>
+        <div class="row text-center contact_row">
+            <div class="contact_row__block">
+                <a href="{{ url('https://lv.linkedin.com/in/waplet') }}">
+                    <img class="contact_row__block__icon" src="{{ asset('images/linkedin.png') }}"/>
+                </a>
+                <div class="contact_row__block__slider slider-linkedin">LinkedIn</div>
+            </div>
+        </div>
+        <div class="row text-center contact_row">
+            <div class="contact_row__block">
+                <a href="{{ url('http://twitter.com/@waplet') }}">
+                    <img class="contact_row__block__icon" src="{{ asset('images/twitter.png') }}"/>
+                </a>
+                <div class="contact_row__block__slider slider-twitter">Twitter</div>
+            </div>
+        </div>
+        <div class="row text-center contact_row">
+            <div class="contact_row__block">
+                <a href="{{ url('http://facebook.com/waplet') }}">
+                    <img class="contact_row__block__icon" src="{{ asset('images/facebook.png') }}"/>
+                </a>
+                <div class="contact_row__block__slider slider-facebook">Facebook</div>
+            </div>
         </div>
     </div>
 @endsection
