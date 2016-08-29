81f5dbae807919d362b99a88a0b6625990990a02
Maris Jankovskis <m.jankovskis@dyninno.lv>
2016-08-29 15:25:14 +0300

    basic show product

--- resources/views/portfolio/show.blade.php
+++ resources/views/portfolio/show.blade.php
@@ -2,4 +2,19 @@
 
 /** $product Product */
 ?>
-{{ dump($product->getFeaturedImage()) }}
+@extends('base')
+
+@section('content')
+<div class="container">
+    <div class="col-xs-12 col-xs-offset-0 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+        <div class="row">
+                <h3>{{ $product->title }}</h3>
+        </div>
+        <div class="row">
+            <div class="col-sm-6">
+                {{ $product->description }}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
