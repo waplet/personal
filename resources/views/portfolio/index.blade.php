6700c23b5f5449df8ddfa332ae8e2bb8ed5ceec4
Maris Jankovskis <maarisjankovskis@gmail.com>
2016-09-18 23:55:23 +0300

    Some updates, but still don't like

--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -3,21 +3,25 @@
 @section('content')
     <div class="container portfolio">
         @forelse ($products as $product)
-        <div class="col-sm-6 col-lg-4">
+        <div class="col-xs-6 col-lg-4 nopadding">
             <div class="portfolio__block">
                 <div class="portfolio__block__image">
                     <a href="{{ url('/portfolio/' . $product->slug) }}">
                         <img src="{{ url($product->getFeaturedImage()) }}"
                              class="img-responsive"/>
                     </a>
+                    <div class="portfolio__block__overlay">
+                        <a class="portfolio__block__overlay__title" href="{{ url('/portfolio/' . $product->slug) }}">{{ $product->title }}</a>
+                        <p class="portfolio__block__overlay__description">{{ $product->description }}</p>
+                    </div>
                 </div>
-                <div class="portfolio__block__info">
-                    <h3 class="portfolio__block__info__title"><a href="{{ url('/portfolio/' . $product->slug) }}" class="">{{ $product->title }}</a></h3>
+                {{--<div class="portfolio__block__info">--}}
+                    {{--<h3 class="portfolio__block__info__title"><a href="{{ url('/portfolio/' . $product->slug) }}" class="">{{ $product->title }}</a></h3>--}}
                     {{--<div class="row">--}}
                         {{--<span class="portfolio__block__info__price {{ $product->price ? 'bg-warning' : 'bg-success' }} btn">{{ $product->price ? $product->price : 'Free'}}</span>--}}
 {{--                        <a href="{{ $product->price ? url('/contact-me') : url($product->url) }}" class="btn btn-default">{{ $product->price ? 'Purchase' : 'Download' }}</a>--}}
                     {{--</div>--}}
-                </div>
+                {{--</div>--}}
             </div>
         </div>
         @empty
